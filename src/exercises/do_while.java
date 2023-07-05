2b870bb6f86a3780b5d2f3057ce973010891572f
rocketpowers <rocketpowerswillian@gmail.com>
2023-07-05 20:14:29 -0300

    do while

---
+++ src/exercises/do_while.java
@@ -0,0 +1,17 @@
+package exercises;
+
+public class do_while {
+
+	public static void main(String[] args) {
+		
+		
+		int cont=1;
+				while(cont<=50) {
+					System.out.println(cont);
+					cont++;
+				}
+		
+
+	}
+
+}
